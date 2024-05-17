26d1458ab56f4766a39647141fd09b001182f480
Sandy <syjy8809@gmail.com>
2024-05-17 17:01:58 +0900

    [FEAT] 이미지 사운드 On Off 에 따라 교체

--- Assets/Scripts/Manager/Managers.cs
+++ Assets/Scripts/Manager/Managers.cs
@@ -7,13 +7,11 @@ using UnityEngine;
 /// </summary>
 public class Managers : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
