cad7cc8e7def9ec935b261055b061cf35d8f462b
kimihea <167047358+kimihea@users.noreply.github.com>
2024-05-17 16:19:31 +0900

    [Feat] Hit and Dead
    피격과 사망 기능 추가

---
+++ Assets/Scripts/Monster/DestroyOnDeath.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.UIElements.Experimental;
+
+public class DestroyOnDeath : MonoBehaviour
+{
+    private MonsterStateController state;
+    private Rigidbody2D rigidbody;
+
+    private void Start()
+    {
+        state = GetComponent<MonsterStateController>();
+        rigidbody = GetComponent<Rigidbody2D>();
+        state.onDeath += OnDeath;
+    }
+
+    void OnDeath()
+    {
+        rigidbody.velocity = Vector3.zero;
+
+        foreach (SpriteRenderer renderer in GetComponentsInChildren<SpriteRenderer>())
+        {
+            Color color = renderer.color;
+            color.a = 0.3f;
+            renderer.color = color;
+        }
+        
+        foreach(Behaviour behaviour in GetComponentsInChildren<Behaviour>())
+        {
+            if(!(behaviour is Animator))
+            behaviour.enabled = false;
+        }
+        // 2초뒤에 파괴
+        Destroy(gameObject, 2f);
+    }
+}
