14ca76a4905383f2e9cc802201215ff1d770a01a
J-Kaun <kangeun9727@gmail.com>
2024-05-17 14:17:03 +0900

    [FEAT] ProjectileController
    ProjectileController 스크립트 작성
    해당 스크립트 구현을 위한 RangedAttackSO , AttackSO 스크립트 작성

---
+++ Assets/Scripts/Projectile/ProjectileController.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileController : MonoBehaviour
+{
+    [SerializeField] private LayerMask levelCollisionLayer;
+
+    private RangedAttackSO attackData;
+    private float currentDuration;
+    private Vector2 direction;
+    private bool isReady;
+
+    private Rigidbody2D rigidbody;
+    private SpriteRenderer spriteRenderer;
+    private TrailRenderer trailRenderer;
+
+    public bool fxOnDestroy = true;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        rigidbody = GetComponent<Rigidbody2D>();
+        trailRenderer = GetComponent<TrailRenderer>();
+    }
+
+    private void Update()
+    {
+        if (!isReady)
+        {
+            return;
+        }
+
+        currentDuration += Time.deltaTime;
+
+        if (currentDuration > attackData.duration)
+        {
+            DestroyProjectile(transform.position, false);
+        }
+
+        rigidbody.velocity = direction * attackData.speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (IsLayerMatched(levelCollisionLayer.value, collision.gameObject.layer))
+        {
+            Vector2 destroyPosition = collision.ClosestPoint(transform.position) - direction * 0.2f;
+            DestroyProjectile(destroyPosition, fxOnDestroy);
+        }
+        else if (IsLayerMatched(levelCollisionLayer.value, collision.gameObject.layer))
+        {
+            // 피격 작성 필요
+            DestroyProjectile(collision.ClosestPoint(transform.position), fxOnDestroy);
+        }
+    }
+
+    private bool IsLayerMatched(int layerMask, int objectLayer)
+    {
+        return layerMask == (layerMask | (1 << objectLayer));
+    }
+
+    public void InitializeAttack(Vector2 direction, RangedAttackSO attackData)
+    {
+        this.attackData = attackData;
+        this.direction = direction;
+
+        UpdateProjectileSprite();
+        trailRenderer.Clear();
+        currentDuration = 0;
+        spriteRenderer.color = attackData.projectileColor;
+
+        transform.right = this.direction;
+
+        isReady = true;
+    }
+
+    private void UpdateProjectileSprite()
+    {
+        transform.localScale = Vector3.one * attackData.size;
+    }
+
+
+    private void DestroyProjectile(Vector3 position, bool createFx)
+    {
+        if (createFx)
+        {
+            // 무기 NameTag로 해당하는 무기 FX 가져오기
+        }
+        
+        gameObject.SetActive(false);
+    }
+}
