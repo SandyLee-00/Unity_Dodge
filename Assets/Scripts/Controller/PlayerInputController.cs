34dc84c0e1c8e0102a4dd12fa4add6d2f593a6fa
PDHyeon <104573747+PDHyeon@users.noreply.github.com>
2024-05-17 10:43:28 +0900

    [UPDATE] Modify Code
    PlayerInputController와 PlayerMovement 스크립트 분할 및
    기능 구현

--- Assets/Scripts/Controller/PlayerInputController.cs
+++ Assets/Scripts/Controller/PlayerInputController.cs
@@ -1,34 +1,28 @@
-﻿using System.Collections;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerInputController : MonoBehaviour
 {
-    private Camera camera;
-    Rigidbody2D movementRigidbody;
-    private Vector2 movementDirection = Vector2.zero;
-    Animator playerAnimator;
+    public event Action<Vector2> OnMoveEvent;
+    public event Action<Vector2> OnLookEvent;
 
-    [SerializeField] float speed = 5.0f;
+    private Camera camera;
+    Animator playerAnimator;
 
     private void Awake()
     {
         camera = Camera.main; // mainCamera태그 붙어있는 카메라 가져옴
-        movementRigidbody = GetComponent<Rigidbody2D>();
         playerAnimator = GetComponentInChildren<Animator>();  
-    }
-
-    private void FixedUpdate()
-    {
-        movementRigidbody.velocity = movementDirection * speed;
     }
 
     private void OnMove(InputValue value)  // 움직임을 처리 (Input Actions Send Messages)
     {
         Vector2 moveInput = value.Get<Vector2>().normalized;
-        movementDirection = moveInput;
-        playerAnimator.SetFloat("Speed", movementDirection.magnitude);
+        playerAnimator.SetFloat("Speed", moveInput.magnitude);
+        CallMoveEvent(moveInput);
     }
 
     private void OnLook(InputValue value)   // 플레이어가 바라보는 방향을 처리 (Input Actions Send Messages)
@@ -37,5 +31,10 @@ public class PlayerInputController : MonoBehaviour
         Vector2 worldPos = camera.ScreenToWorldPoint(newAim);
 
         newAim = (worldPos - (Vector2)transform.position).normalized;       // worldPos - 플레이어 위치 계산 시 해당 지점을 바라보는 벡터를 구할 수 있음.
+    }
+
+    public void CallMoveEvent(Vector2 direction)
+    {
+        OnMoveEvent?.Invoke(direction); // ? 없으면 말고 있으면 실행
     }
 }
