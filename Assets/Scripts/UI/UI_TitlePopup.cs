703dccd3e7fb0eae5e68dcf2eb5aa3d91894aa90
Sandy <syjy8809@gmail.com>
2024-05-17 11:29:11 +0900

    [FEAT] OptionPopup 배치해서 프리팹 만들기

--- Assets/Scripts/UI/UI_TitlePopup.cs
+++ Assets/Scripts/UI/UI_TitlePopup.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UI_TitlePopup : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Button StartButton;
+    Button ExitButton;
+    Button OptionButton;
+
+    private void Awake()
     {
-        
+        StartButton = transform.Find("StartButton").GetComponent<Button>();
+        ExitButton = transform.Find("ExitButton").GetComponent<Button>();
+        OptionButton = transform.Find("OptionButton").GetComponent<Button>();
+
+        // 플레이 씬으로 이동하기
+        StartButton.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene("Play");
+        });
+
+        // 게임 종료하기
+        ExitButton.onClick.AddListener(() =>
+        {
+            Application.Quit();
+        });
+
+        // 옵션 팝업 띄우기
+        OptionButton.onClick.AddListener(() =>
+        {
+            // TODO : Managers에 UI 매니저 추가하기 
+            // Managers.UI.ShowPopup<UI_OptionPopup>();
+        });
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
