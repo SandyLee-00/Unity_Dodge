e073158730cb50d3099694f1ffa10e716fe0fdd6
kimihea <167047358+kimihea@users.noreply.github.com>
2024-05-21 18:28:43 +0900

    [Fix]Enemy3, [Fix]Push Obstacle

--- Assets/Scripts/Entities/Monster/MonsterControl/MonsterAim.cs
+++ Assets/Scripts/Entities/Monster/MonsterControl/MonsterAim.cs
@@ -18,21 +18,21 @@ public class MonsetAim : MonoBehaviour
     }
     private void OnAim(Vector2 direction)
     {
-        if (!controller.isRange)
+        if (controller.isRange)
         {
             ApplyLook(direction);
-            FlipWeapon(direction);
+            RotateWeapon(direction);
         }
         else
         {
             ApplyLook(direction);
-            RotateWeapon(direction);
+            FlipWeapon(direction);
         }
         
     }
     //Vector2 flipPivotPosition = new Vector2(-0.432f, 0.04f);
     //Vector2 originPosition = new Vector2(0.531f,-0.025f);
-    //Enemy3 Vector2(0.748,-0.909)/ (-0.138,-0.08)
+    //Enemy3 Vector2(-0.33,-0.24)/ (0.4,-0.24)
     private void ApplyLook(Vector2 direction)
     {
         float rotZ = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
@@ -40,8 +40,14 @@ public class MonsetAim : MonoBehaviour
     }
     private void RotateWeapon(Vector2 direction)
     {
-         float rotZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-         armPivot.rotation = Quaternion.Euler(0, 0, rotZ);
+        float rotZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        armPivot.rotation = Quaternion.Euler(0, 0, rotZ);
+        bool flip = Mathf.Abs(rotZ) > 90f;
+        armRenderer.flipY = flip;
+        if (flip)
+            armPivot.localPosition = flipPivotPosition;
+        else
+            armPivot.localPosition = originPosition;
     }
     private void FlipWeapon(Vector2 direction)
     {
@@ -50,9 +56,7 @@ public class MonsetAim : MonoBehaviour
         if (flip)
         {
             armPivot.rotation = Quaternion.Euler(new Vector3(0,0,51f));
-            
-            armRenderer.flipX = true;
-            
+            armRenderer.flipX = true; 
             armPivot.localPosition = flipPivotPosition;
             //armRenderer.transform.localRotation = Quaternion.Euler(0, 0, 60f);
         }
@@ -62,7 +66,6 @@ public class MonsetAim : MonoBehaviour
             armRenderer.flipX = false;
             armPivot.localPosition = originPosition;
             //armRenderer.transform.localRotation = Quaternion.Euler(0, 0, -22f);
-
         }
     }
 }
