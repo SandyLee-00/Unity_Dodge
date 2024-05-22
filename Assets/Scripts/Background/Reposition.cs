8d9d04a466925ad9aa3bb3595dcf429e02303e6c
Sandy <syjy8809@gmail.com>
2024-05-22 16:33:12 +0900

    [FEAT] 무한 맵이동

---
+++ Assets/Scripts/Background/Reposition.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEditor.PlayerSettings;
+
+public class Reposition : MonoBehaviour
+{
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Area"))
+        {
+            return;
+        }
+
+        if(Managers.Game.IsGameEnd)
+        {
+            return;
+        }
+
+        Vector3 playerPos = Managers.Game.Player.transform.position;
+        Vector3 myPos = transform.position;
+
+        float dirX = playerPos.x - myPos.x;
+        float dirY = playerPos.y - myPos.y;
+
+        float diffx = Mathf.Abs(dirX);
+        float diffy = Mathf.Abs(dirY);
+
+        dirX = dirX > 0 ? 1 : -1;
+        dirY = dirY > 0 ? 1 : -1;
+
+
+        if (transform.tag == "Ground")
+        {
+            if (diffx >= diffy)
+            {
+                transform.Translate(Vector3.right * dirX * 40);
+            }
+            else
+            {
+                 transform.Translate(Vector3.up * dirY * 40);
+            }
+        }
+    }
+
+}
